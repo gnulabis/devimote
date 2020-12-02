d25502bddafaad15e35a057033b57b99afb010ff
Dimitris Lampridis <dimitris.lampridis@cern.ch>
2020-12-02 11:17:58 +0100

    upload wireshark LUA dissectors

---
+++ wireshark/dec.lua
@@ -0,0 +1,96 @@
+dec_protocol = Proto("DEC",  "Devialet Expert Control")
+
+dec_protocol.fields.magic_val   = ProtoField.string("dec.magic",      "Magic Value")
+dec_protocol.fields.packet_cnt  = ProtoField.uint16("dec.packet_cnt", "Packet Counter")
+dec_protocol.fields.cmd_cnt     = ProtoField.uint16("dec.cmd_cnt",    "Command Counter")
+dec_protocol.fields.cmd_type    = ProtoField.string("dec.cmd_type",   "Command")
+dec_protocol.fields.out_sel     = ProtoField.uint16("dec.out_sel",    "Output Select")
+dec_protocol.fields.volume      = ProtoField.uint16("dec.volume",     "Volume", base.HEX)
+dec_protocol.fields.crc16_check = ProtoField.string("dec.crc_check",  "Checksum")
+
+dec_protocol.fields.todo_field = ProtoField.none("dec.todo", "Unknown Field")
+dec_protocol.fields.zero_field = ProtoField.none("dec.zero", "Zero Padding")
+
+function dec_protocol.dissector(buffer, pinfo, tree)
+
+   length = buffer:len()
+   if length == 0 then return end
+
+   pinfo.cols.protocol = dec_protocol.name
+
+   local subtree = tree:add(dec_protocol, buffer(), dec_protocol.description)
+   subtree:add(dec_protocol.fields.magic_val,   buffer(0x0,2))
+   subtree:add(dec_protocol.fields.packet_cnt,  buffer(0x2,2))
+   subtree:add(dec_protocol.fields.cmd_cnt,     buffer(0x4,2))
+
+   local opstring = "UNKNOWN"
+   local opcode = buffer(0x6,2):uint()
+
+   if     (opcode == 0x0004) then opstring = "VOLUME"
+   elseif (opcode == 0x0005) then opstring = "OUTPUT"
+   elseif (opcode == 0x0001) then opstring = "STANDBY"
+   elseif (opcode == 0x0101) then opstring = "ON"
+   elseif (opcode == 0x0107) then opstring = "MUTE"
+   elseif (opcode == 0x0007) then opstring = "UNMUTE"
+   end
+
+   subtree:add(dec_protocol.fields.cmd_type, buffer(0x6,2), opstring)
+
+   if (opstring == "VOLUME") then
+      local volume = buffer(0x8,2):uint()
+      subtree:add(dec_protocol.fields.volume, volume)
+      subtree:add(dec_protocol.fields.zero_field, buffer(0xa,2))
+      pinfo.cols.info:append(string.format(" Cmd=%s %x", opstring, volume))
+   elseif (opstring == "OUTPUT SELECT") then
+      local out_sel = bit32.band(buffer(0x8,2):uint(), 0x01ff)
+      if (bit32.band(out_sel, 0x100) ~= 0) then
+         out_sel = bit32.band(out_sel, 0xff)
+         out_sel = bit32.lshift(out_sel, 1)
+         out_sel = bit32.bor(out_sel, 0x100)
+      end
+      out_sel = bit32.rshift(out_sel, 5)
+      subtree:add(dec_protocol.fields.out_sel, buffer(0x8,2), out_sel)
+      subtree:add(dec_protocol.fields.zero_field, buffer(0xa,2))
+      pinfo.cols.info:append(string.format(" Cmd=%s #%02d", opstring, out_sel))
+   else
+      subtree:add(dec_protocol.fields.zero_field, buffer(0x8,4))
+      pinfo.cols.info:append(string.format(" Cmd=%s", opstring))
+   end
+
+   -- ccitt_16() taken from https://github.com/clarkli86/crc16_ccitt
+   -- loop adjusted for wireshark Tvbrange as argument
+   local function ccitt_16(byte_array)
+
+      local POLY = 0x1021
+
+      local function hash(crc, byte)
+         for i = 0, 7 do
+            local bit = bit32.extract(byte, 7 - i) -- Take the lsb
+            local msb = bit32.extract(crc, 15, 1) -- msb
+            crc = bit32.lshift(crc, 1) -- Remove the lsb of crc
+            if bit32.bxor(bit, msb) == 1 then crc = bit32.bxor(crc, POLY) end
+         end
+         return crc
+      end
+
+      local crc = 0xffff
+      for i = 0, byte_array:len()-1, 1 do
+         crc = hash(crc, byte_array:range(i,1):uint())
+      end
+
+      return bit32.extract(crc, 0, 16)
+   end
+
+   local crc16_calc = ccitt_16(buffer(0x0,12))
+   local crc16_read = buffer(0xc,2):uint()
+   subtree:add(dec_protocol.fields.crc16_check, buffer(0xc,2), crc16_calc == crc16_read and "Valid" or "Invalid")
+   -- local crc16_calc = ccitt_16(buffer(0xe,126))
+   -- local crc16_read = buffer(0x8c,2):uint()
+   -- subtree:add(dec_protocol.fields.crc16_data, buffer(0xe,126), crc16_calc == crc16_read and "Valid" or "Invalid")
+
+   subtree:add(dec_protocol.fields.todo_field, buffer(0xe,128))
+
+end
+
+local udp_port = DissectorTable.get("udp.port")
+udp_port:add(45455, dec_protocol)
---
+++ wireshark/des.lua
@@ -0,0 +1,108 @@
+des_protocol = Proto("DES",  "Devialet Expert Status")
+
+des_protocol.fields.magic_val   = ProtoField.string("des.magic",      "Magic Value")
+des_protocol.fields.packet_cnt  = ProtoField.uint16("des.packet_cnt", "Packet Counter")
+des_protocol.fields.dev_name    = ProtoField.string("des.dev_name",   "Device Name")
+des_protocol.fields.dev_uptime  = ProtoField.string("des.dev_uptime", "Uptime")
+des_protocol.fields.crc16_check = ProtoField.string("des.crc_check",  "Checksum")
+des_protocol.fields.status      = ProtoField.string("des.status",     "Status")
+des_protocol.fields.volume      = ProtoField.string("des.status",     "Volume")
+
+des_protocol.fields.todo_field1 = ProtoField.none("des.todo1", "Unknown Field")
+des_protocol.fields.todo_field2 = ProtoField.none("des.todo2", "Unknown Field")
+des_protocol.fields.todo_field3 = ProtoField.none("des.todo3", "Unknown Field")
+des_protocol.fields.todo_field4 = ProtoField.none("des.todo4", "Unknown Field")
+des_protocol.fields.todo_field5 = ProtoField.none("des.todo5", "Unknown Field")
+
+output_channel = {}
+
+for i = 0, 14, 1 do
+   output_channel[i] = ProtoField.string(string.format("des.out%d", i), string.format("Output #%02d", i))
+   table.insert(des_protocol.fields, output_channel[i])
+end
+
+
+function des_protocol.dissector(buffer, pinfo, tree)
+
+   length = buffer:len()
+   if length == 0 then return end
+
+   pinfo.cols.protocol = des_protocol.name
+
+   local subtree = tree:add(des_protocol, buffer(), des_protocol.description)
+   subtree:add(des_protocol.fields.magic_val,   buffer(0x0,2))
+   subtree:add(des_protocol.fields.packet_cnt,  buffer(0x2,2))
+   subtree:add(des_protocol.fields.todo_field1, buffer(0x4,15))
+   subtree:add(des_protocol.fields.dev_name,    buffer(0x013,31))
+   subtree:add(des_protocol.fields.todo_field2, buffer(0x32,2))
+
+   for i = 0, 14, 1 do
+      local out_val = string.format("%s [%s]", buffer(0x35+17*i, 16):string(), buffer(0x34+17*i, 1):uint() ~= 0x30 and "Enabled" or "Disabled")
+      subtree:add(output_channel[i], buffer(0x34+17*i,17), out_val)
+      --local out_stat = 
+      --local out_name = 
+      --outputSubtree:add(output_stat[i], out_stat, out_stat:uint() ~= 0x30 and "Enabled" or "Disabled")
+      --outputSubtree:add(output_name[i], out_name)
+   end
+
+   local function disp_status(stat)
+      local power    = (bit32.band(stat, 0x8000) ~= 0) and "On" or "Standby"
+      local muted    = (bit32.band(stat, 0x0002) ~= 0) and "Yes" or "No"
+      local out_num  = bit32.rshift(bit32.band(stat, 0x003c), 2)
+      local out_name = buffer(0x35+17*out_num, 16):string()
+      return string.format("Power: %s, Output: %s, Muted: %s", power, out_name, muted)
+   end
+
+   subtree:add(des_protocol.fields.status, buffer(0x133,2), disp_status(buffer(0x133,2):uint()))
+
+   subtree:add(des_protocol.fields.todo_field3, buffer(0x135,1))
+
+   local vol = (buffer(0x136,1):uint() - 195) / 2.0
+   subtree:add(des_protocol.fields.volume, buffer(0x136,1), string.format("%02.1f dB", vol))
+
+   subtree:add(des_protocol.fields.todo_field4, buffer(0x137,20))
+
+   local function disp_uptime(time)
+      local days = math.floor(time / 86400000)
+      local hours = math.floor((time % 86400000)/3600000)
+      local minutes = math.floor((time % 3600000)/60000)
+      local seconds = math.floor((time % 60000)/1000)
+      local milli = math.floor(time % 1000)
+      return string.format("%d days, %02d hours, %02d minutes, %02d sec, %03d ms", days, hours, minutes, seconds, milli)
+   end
+
+   subtree:add(des_protocol.fields.dev_uptime, buffer(0x014b,4), disp_uptime(buffer(0x014b,4):uint()))
+
+   -- ccitt_16() taken from https://github.com/clarkli86/crc16_ccitt
+   -- loop adjusted for wireshark Tvbrange as argument
+   local function ccitt_16(byte_array)
+
+      local POLY = 0x1021
+
+      local function hash(crc, byte)
+         for i = 0, 7 do
+            local bit = bit32.extract(byte, 7 - i) -- Take the lsb
+            local msb = bit32.extract(crc, 15, 1) -- msb
+            crc = bit32.lshift(crc, 1) -- Remove the lsb of crc
+            if bit32.bxor(bit, msb) == 1 then crc = bit32.bxor(crc, POLY) end
+         end
+         return crc
+      end
+
+      local crc = 0xffff
+      for i = 0, byte_array:len()-1, 1 do
+         crc = hash(crc, byte_array:range(i,1):uint())
+      end
+
+      return bit32.extract(crc, 0, 16)
+   end
+
+   subtree:add(des_protocol.fields.todo_field5, buffer(0x14f,8))
+
+   local crc16_calc = ccitt_16(buffer(0x0,343))
+   local crc16_read = buffer(0x157,2):uint()
+   subtree:add(des_protocol.fields.crc16_check, buffer(0x157,2), crc16_calc == crc16_read and "Valid" or "Invalid")
+end
+
+local udp_port = DissectorTable.get("udp.port")
+udp_port:add(45454, des_protocol)
